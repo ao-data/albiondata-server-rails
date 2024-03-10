f03228de250fc6a5389a849221debccc25535e82
Paul Hendryx <phendryx@gmail.com>
2024-03-10 00:21:46 -0500

    config stuff

---
+++ config/initializers/zeitwork.rb
@@ -0,0 +1,5 @@
+Rails.autoloaders.each do |autoloader|
+  # autoloader.ignore(Rails.root.join('mysqldata'))
+  # autoloader.ignore(Rails.root.join('mysqlbackup'))
+  # autoloader.ignore(Rails.root.join('redisdata'))
+end
