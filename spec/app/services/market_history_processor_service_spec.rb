c2b0ca188b263c46f5e39101bd300fbd7b7473e4
Paul Hendryx <phendryx@gmail.com>
2024-04-13 20:05:15 -0400

    lets try market history timestamp again...

--- spec/app/services/market_history_processor_service_spec.rb
+++ spec/app/services/market_history_processor_service_spec.rb
@@ -3,8 +3,8 @@ describe MarketHistoryProcessorService, type: :service do
   describe '.ticks_to_time' do
 
     it 'converts ticks to time object' do
-      ticks = 638486424000000000
-      expected_datetime = DateTime.parse("2023-04-11 22:00:00 +0000")
+      ticks = 638486460000000000
+      expected_datetime = DateTime.parse("2024-04-13 23:00:00 +0000")
       expect(MarketHistoryProcessorService.ticks_to_time(ticks)).to eq(expected_datetime)
     end
   end
