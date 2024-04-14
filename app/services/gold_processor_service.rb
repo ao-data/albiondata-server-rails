3e902e69d6045dda3a87a1deccbf524f756a8f07
Paul Hendryx <phendryx@gmail.com>
2024-04-14 08:51:53 -0400

    file rename, gold processing time conversion, tests

--- app/services/gold_processor_service.rb
+++ app/services/gold_processor_service.rb
@@ -1,7 +1,7 @@
 class GoldProcessorService
   def self.process(data)
     data['Prices'].each_with_index do |price, index|
-      timestamp = Time.at((data['Timestamps'][index] / 10000 - 62136892800000) / 1000)
+      timestamp = Time.at((data['Timestamps'][index] - 621355968000000000)/10000000)
       GoldPrice.find_or_create_by(price: price, timestamp: timestamp)
     end
   end
