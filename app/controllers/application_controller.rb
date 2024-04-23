ba8b10786d81c826b79abed3c3b03e26f36619dc
Paul Hendryx <phendryx@gmail.com>
2024-04-22 21:15:37 -0400

    application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  around_action :run_using_database
+
+  def test
+    # this only here for spec testing run_using_database
+    render json: { orders: MarketOrder.all }.to_json
+  end
+
+  def server_id
+    request.subdomain
+  end
+
+  def run_using_database(&block)
+    pp "using database #{request.subdomain}"
+    Multidb.use(server_id, &block)
+  end
 end
---
+++ spec/app/controllers/application_controller_spec.rb
@@ -0,0 +1,134 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, :type => :controller do
+
+  before do
+    @request.host = "west.example.com"
+
+    [:east, :west, :europe].each do |db|
+      Multidb.use(db) do
+        MarketOrder.delete_all
+      end
+    end
+  end
+
+  it 'east db items are not available in west/europe dbs' do
+    Multidb.use(:east) do
+      create(:market_order)
+    end
+
+    Multidb.use(:west) do
+      expect(MarketOrder.all).to be_empty
+    end
+
+    Multidb.use(:europe) do
+      expect(MarketOrder.all).to be_empty
+    end
+  end
+
+  it 'west db items are not available in east/europe dbs' do
+    Multidb.use(:west) do
+      create(:market_order)
+    end
+
+    Multidb.use(:east) do
+      expect(MarketOrder.all).to be_empty
+    end
+
+    Multidb.use(:europe) do
+      expect(MarketOrder.all).to be_empty
+    end
+  end
+
+  it 'europe db items are not available in east/west dbs' do
+    Multidb.use(:europe) do
+      create(:market_order)
+    end
+
+    Multidb.use(:east) do
+      expect(MarketOrder.all).to be_empty
+    end
+
+    Multidb.use(:west) do
+      expect(MarketOrder.all).to be_empty
+    end
+  end
+
+  describe 'when using subdomains' do
+    describe 'and there is data for that subdomain' do
+      it 'returns the data for that subdomain' do
+        Multidb.use(:east) do
+          create(:market_order)
+        end
+
+        @request.host = "east.example.com"
+        get :test
+        expect(JSON.parse(response.body)['orders'].length).to eq(1)
+      end
+    end
+
+    it 'does not return data for other subdomains' do
+      Multidb.use(:east) do
+        create(:market_order)
+      end
+
+      Multidb.use(:europe) do
+        create(:market_order)
+      end
+
+      @request.host = "west.example.com"
+      get :test
+      expect(JSON.parse(response.body)['orders']).to be_empty
+    end
+
+    it 'returns data for east subdomain' do
+      Multidb.use(:east) do
+        create(:market_order)
+      end
+
+      @request.host = "east.example.com"
+      get :test
+      expect(JSON.parse(response.body)['orders'].length).to eq(1)
+    end
+
+    it 'returns data for europe subdomain' do
+      Multidb.use(:europe) do
+        create(:market_order)
+      end
+
+      @request.host = "europe.example.com"
+      get :test
+      expect(JSON.parse(response.body)['orders'].length).to eq(1)
+    end
+
+    it 'returns data for west subdomain' do
+      Multidb.use(:west) do
+        create(:market_order)
+      end
+
+      @request.host = "west.example.com"
+      get :test
+      expect(JSON.parse(response.body)['orders'].length).to eq(1)
+    end
+
+    it 'defaults to west if unknown subdomain' do
+      Multidb.use(:west) do
+        create(:market_order)
+      end
+
+      Multidb.use(:east) do
+        create(:market_order)
+        create(:market_order)
+      end
+
+      Multidb.use(:europe) do
+        create(:market_order)
+        create(:market_order)
+      end
+
+      @request.host = "unknown.example.com"
+      get :test
+      expect(JSON.parse(response.body)['orders'].length).to eq(1)
+    end
+  end
+end
