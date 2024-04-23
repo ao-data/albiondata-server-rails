715f30b483e28ec8a7196c005141823ed186fde8
Paul Hendryx <phendryx@gmail.com>
2024-04-22 22:02:34 -0400

    market history cleanup worker

--- app/workers/market_history_cleanup_worker.rb
+++ app/workers/market_history_cleanup_worker.rb
@@ -2,7 +2,9 @@ class MarketHistoryCleanupWorker
   include Sidekiq::Job
   sidekiq_options queue: :low
 
-  def perform
-    MarketHistory.purge_old_data
+  def perform(server_id)
+    Multidb.use(server_id.to_sym) do
+      MarketHistory.purge_old_data
+    end
   end
 end
--- spec/app/workers/market_history_cleanup_worker_spec.rb
+++ spec/app/workers/market_history_cleanup_worker_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe MarketHistoryCleanupWorker, :type => :worker do
 
   it 'calls MarketHistory.purge_old_data' do
     expect(MarketHistory).to receive(:purge_old_data)
-    subject.perform
+    subject.perform('west')
+  end
+
+  it 'uses the correct database' do
+    expect(Multidb).to receive(:use).with(:east).and_call_original
+    subject.perform('east')
   end
 
 end
