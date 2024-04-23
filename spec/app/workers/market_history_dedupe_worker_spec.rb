fb3c006cbb020d11ce520b6ba72c0255a415031d
Paul Hendryx <phendryx@gmail.com>
2024-04-22 22:07:06 -0400

    market history dedupe worker

--- spec/app/workers/market_history_dedupe_worker_spec.rb
+++ spec/app/workers/market_history_dedupe_worker_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe MarketHistoryDedupeWorker, :type => :worker do
 
   it 'calls MarketHistoryDedupeService.dedupe' do
     data = { 'foo' => 'bar' }
-    expect(MarketHistoryDedupeService).to receive(:dedupe).with(data)
-    subject.perform(data.to_json)
+    expect(MarketHistoryDedupeService).to receive(:dedupe).with(data, 'west')
+    subject.perform(data.to_json, 'west')
   end
 end
