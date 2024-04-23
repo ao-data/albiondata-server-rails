09afb0f6c27301f49d832dba517c38eb8b40459a
Paul Hendryx <phendryx@gmail.com>
2024-04-22 21:57:13 -0400

    gold dedupe worker

--- spec/app/workers/gold_dedupe_worker_spec.rb
+++ spec/app/workers/gold_dedupe_worker_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe GoldDedupeWorker, :type => :worker do
 
   it 'calls GoldDedupeService.dedupe' do
     data = { 'foo' => 'bar' }
-    expect(GoldDedupeService).to receive(:dedupe).with(data)
-    subject.perform(data.to_json)
+    expect(GoldDedupeService).to receive(:dedupe).with(data, 'west')
+    subject.perform(data.to_json, 'west')
   end
 
 end
