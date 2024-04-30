5834e8102cb7185c117292796de669eac72b2a65
Paul Hendryx <phendryx@gmail.com>
2024-04-29 20:19:41 -0400

    reduce api load by prebuilding a list of humanized cities

--- app/services/market_data_service.rb
+++ app/services/market_data_service.rb
@@ -93,39 +93,57 @@ class MarketDataService
                              'location',
                              'quality_level')
 
+    # build a hash of humanized cities for this call
+    humanized_cities = {}
+    locations.each do |location|
+      humanized_cities[location] = humanize_city(location_to_city(location).to_s)
+    end
+
+    rows = []
+    execution_time = Benchmark.measure do
+      rows = MarketOrder.connection.select_rows(data.to_sql)
+    end
+    puts "Retriving data took #{execution_time.real} seconds"
+
     results = {}
     default_date = DateTime.new(0001, 1, 1, 0, 0, 0).strftime('%Y-%m-%dT%H:%M:%S')
     last_o_keey_at = nil
-    MarketOrder.connection.select_rows(data.to_sql).each do |result|
-      o_keey, o_keey_at, updated_at_binned, min_price, max_price, auction_type, item_id, location, quality_level = result
+    execution_time = Benchmark.measure do
+      rows.each do |result|
+        o_keey, o_keey_at, updated_at_binned, min_price, max_price, auction_type, item_id, location, quality_level = result
 
-      humanized_city = humanize_city(location_to_city(location).to_s)
-      city_key = o_keey.gsub("!!#{location.to_s}!!", "!!#{humanized_city}!!")
+        humanized_city = humanized_cities[location]
+        city_key = o_keey.gsub("!!#{location.to_s}!!", "!!#{humanized_city}!!")
 
-      next if last_o_keey_at == o_keey_at
-      last_o_keey_at = o_keey_at
+        next if last_o_keey_at == o_keey_at
+        last_o_keey_at = o_keey_at
 
-      results[city_key] ||= {item_id: item_id, city: humanized_city, quality: quality_level, sell_price_min: 0, sell_price_min_date: default_date, sell_price_max: 0, sell_price_max_date: default_date, buy_price_min: 0, buy_price_min_date: default_date, buy_price_max: 0, buy_price_max_date: default_date }
-      if auction_type == 'offer'
-        results[city_key][:sell_price_min] = min_price
-        results[city_key][:sell_price_min_date] = updated_at_binned
-        results[city_key][:sell_price_max] = max_price
-        results[city_key][:sell_price_max_date] = updated_at_binned
-      else
-        results[city_key][:buy_price_min] = min_price
-        results[city_key][:buy_price_min_date] = updated_at_binned
-        results[city_key][:buy_price_max] = max_price
-        results[city_key][:buy_price_max_date] = updated_at_binned
+        results[city_key] ||= {item_id: item_id, city: humanized_city, quality: quality_level, sell_price_min: 0, sell_price_min_date: default_date, sell_price_max: 0, sell_price_max_date: default_date, buy_price_min: 0, buy_price_min_date: default_date, buy_price_max: 0, buy_price_max_date: default_date }
+        if auction_type == 'offer'
+          results[city_key][:sell_price_min] = min_price
+          results[city_key][:sell_price_min_date] = updated_at_binned
+          results[city_key][:sell_price_max] = max_price
+          results[city_key][:sell_price_max_date] = updated_at_binned
+        else
+          results[city_key][:buy_price_min] = min_price
+          results[city_key][:buy_price_min_date] = updated_at_binned
+          results[city_key][:buy_price_max] = max_price
+          results[city_key][:buy_price_max_date] = updated_at_binned
+        end
       end
     end
+    puts "Data processing took #{execution_time.real} seconds"
 
     # fill in the blanks
-    default_values = { sell_price_min: 0, sell_price_min_date: default_date, sell_price_max: 0, sell_price_max_date: default_date, buy_price_min: 0, buy_price_min_date: default_date, buy_price_max: 0, buy_price_max_date: default_date }
-    location_strings = locations.map { |location| location_to_city(location) }
-    ids.sort.product(location_strings.map{|s| humanize_city(s.to_s)}.sort, qualities.sort).map do |item_id, location, quality|
-      key = "#{item_id}!!#{location}!!#{quality}"
-      results[key] ||= {item_id: item_id, city: location, quality: quality}.merge(default_values)
+    execution_time = Benchmark.measure do
+      default_values = { sell_price_min: 0, sell_price_min_date: default_date, sell_price_max: 0, sell_price_max_date: default_date, buy_price_min: 0, buy_price_min_date: default_date, buy_price_max: 0, buy_price_max_date: default_date }
+      location_strings = locations.map { |location| location_to_city(location) }
+      ids.sort.product(location_strings.map{|s| humanize_city(s.to_s)}.sort, qualities.sort).map do |item_id, location, quality|
+        key = "#{item_id}!!#{location}!!#{quality}"
+        results[key] ||= {item_id: item_id, city: location, quality: quality}.merge(default_values)
+      end
     end
+    puts "Blanks processing took #{execution_time.real} seconds"
 
     results.values_at(*results.keys.sort)
   end
