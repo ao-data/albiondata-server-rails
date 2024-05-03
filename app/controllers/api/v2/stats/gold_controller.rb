c3fb642c19e0e3eefe58644224cf1b5968809222
Paul Hendryx <phendryx@gmail.com>
2024-05-03 07:30:02 -0400

    fix gold api sort direction

--- app/controllers/api/v2/stats/gold_controller.rb
+++ app/controllers/api/v2/stats/gold_controller.rb
@@ -5,7 +5,7 @@ class API::V2::Stats::GoldController < API::V2::APIController
       date_from = params.key?(:date) ? Date.strptime(params[:date], '%m-%d-%Y') : DateTime.now - 1.month
       date_to = (params.key?(:end_date) ? Date.strptime(params[:end_date], '%m-%d-%Y') : DateTime.now) + 1.day
 
-      results = GoldPrice.where(timestamp: date_from..date_to).order(timestamp: :desc).map do |row|
+      results = GoldPrice.where(timestamp: date_from..date_to).order(timestamp: :asc).map do |row|
         data = { price: row[:price] / 10000, timestamp: row[:timestamp].strftime('%Y-%m-%dT%H:%M:%S') }
         data.merge!({ id: row[:id] }) if request.format == :xml
         data
