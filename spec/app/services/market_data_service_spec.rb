ad4bbdaf1daa3fec3db965e559535570e3ba0229
Albion Free Market <albionfreemarket@gmail.com>
2024-05-23 23:31:35 -0300

    Remove #get_qualities

--- spec/app/services/market_data_service_spec.rb
+++ spec/app/services/market_data_service_spec.rb
@@ -183,35 +183,4 @@ RSpec.describe MarketDataService, :type => :service do
     end
   end
 
-  describe '#get_qualities' do
-    it 'returns default list of qualities if qualities is nil' do
-      params = { qualities: nil}
-      expect(subject.get_qualities(params)).to eq([1])
-    end
-
-    it 'returns default list of qualities if qualities is 0' do
-      params = { qualities: 0}
-      expect(subject.get_qualities(params)).to eq([1,2,3,4,5])
-    end
-
-    it 'returns empty list of qualities if qualities is an empty string' do
-      params = { qualities: ''}
-      expect(subject.get_qualities(params)).to eq([])
-    end
-
-    it 'returns empty list of qualities if qualities is an empty array' do
-      params = { qualities: []}
-      expect(subject.get_qualities(params)).to eq([])
-    end
-
-    it 'returns string converted qualities if qualities is a string' do
-      params = { qualities: '1,2'}
-      expect(subject.get_qualities(params)).to eq([1,2])
-    end
-
-    it 'returns string converted qualities if qualities is an array' do
-      params = { qualities: ['1', '2']}
-      expect(subject.get_qualities(params)).to eq([1,2])
-    end
-  end
 end
