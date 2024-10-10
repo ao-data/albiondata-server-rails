f343cac7f1b57532f8181e3b0e261a876abbd604
Paul Hendryx <phendryx@gmail.com>
2024-10-10 13:02:05 -0400

    optional deletion of data on market history export worker with test

--- app/workers/market_history_export_worker.rb
+++ app/workers/market_history_export_worker.rb
@@ -1,7 +1,13 @@
 class MarketHistoryExportWorker
   include Sidekiq::Worker
 
-  def perform (server_id, year = nil, month = nil)
+  def perform (server_id, year = nil, month = nil, delete_data = false)
     MarketHistoryExportService.export(server_id, year, month)
+
+    if delete_data
+      Multidb.use(server_id.to_sym) do
+        MarketHistory.where("timestamp between ? and ?", Time.new(year, month, 1), Time.new(year, month, 1).end_of_month).delete_all
+      end
+    end
   end
 end
--- spec/app/workers/market_history_export_worker_spec.rb
+++ spec/app/workers/market_history_export_worker_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe MarketHistoryExportWorker, :type => :worker do
     expect(MarketHistoryExportService).to receive(:export).with('west', '2024', '01')
     subject.perform('west', '2024', '01')
   end
+
+  it 'deletes data if delete_data is true' do
+    Timecop.freeze(Time.new(2024, 1, 1)) do
+      Multidb.use(:west) do
+        create(:market_history, timestamp: Time.new(2023, 12, 1))
+        create(:market_history, timestamp: Time.new(2024, 1, 1))
+        create(:market_history, timestamp: Time.new(2024, 2, 1))
+      end
+      expect(MarketHistoryExportService).to receive(:export).with('west', '2024', '01')
+      subject.perform('west', '2024', '01', true)
+      Multidb.use(:west) do
+        expect(MarketHistory.count).to eq(2)
+        expect(MarketHistory.where("timestamp between '2024-01-01' and '2024-01-31'").count).to eq(0)
+      end
+    end
+  end
 end
