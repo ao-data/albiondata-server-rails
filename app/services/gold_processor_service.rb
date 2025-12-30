509531b528e88c5a7518e6c952ad2d8f4c09518c
Paul Hendryx <phendryx@gmail.com>
2025-12-30 07:42:56 -0500

    fix gold processing to stop inserting duplicates

--- app/services/gold_processor_service.rb
+++ app/services/gold_processor_service.rb
@@ -7,7 +7,12 @@ class GoldProcessorService
     Multidb.use(server_id.to_sym) do
       data['Prices'].each_with_index do |price, index|
         timestamp = Time.at((data['Timestamps'][index] - 621355968000000000)/10000000)
-        GoldPrice.find_or_create_by(price: price, timestamp: timestamp)
+        gold_price = GoldPrice.find_by(timestamp: timestamp)
+        if gold_price
+          gold_price.update(price: price)
+        else
+          GoldPrice.create(price: price, timestamp: timestamp)
+        end
       end
     end
     IdentifierService.add_identifier_event(opts, server_id, 'Saved to database from GoldProcessorService')
