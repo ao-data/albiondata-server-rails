7f4eb2ff6bce5a10fd103572b7a0e0a4dccae71b
Daniel Koch <koch@triple6.org>
2026-01-30 20:44:58 +0100

    validations

--- app/services/bandit_event_service.rb
+++ app/services/bandit_event_service.rb
@@ -1,6 +1,10 @@
 class BanditEventService
   def process(data, server_id, opts)
-    json_data = data.slice("EventTime").to_json
+    # Validations
+    data = data.slice("EventTime") # We only need a key of EventTime
+    data["EventTime"] = data["EventTime"].to_i # EventTime will always be an integer
+
+    json_data = data.to_json
     json_opts = opts.to_json
 
     nats = NatsService.new(server_id)
