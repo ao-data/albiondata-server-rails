8931d9283c9b0a918c6c0542ce4407509d62f025
Daniel Koch <koch@triple6.org>
2026-01-31 18:12:56 +0100

    update bandit_event_service

--- app/services/bandit_event_service.rb
+++ app/services/bandit_event_service.rb
@@ -1,19 +1,7 @@
 class BanditEventService
   def process(data, server_id, opts)
-    # Validations
-    # Expected: { 'EventTime' => 638997538711438026 }
-    data = data.slice('EventTime') # We only need a key of EventTime
-    data['EventTime'] = data['EventTime'].to_i # EventTime will always be an integer
-
     log = { class: 'BanditEventService', method: 'process', data: data, server_id: server_id, opts: opts }
     Sidekiq.logger.info(log.to_json)
-
-    if data['EventTime'] == 0 # Don't forward if data does not match schema
-      err = 'Discarded. Did not match expected schema'
-      Sidekiq.logger.warn(err)
-      IdentifierService.add_identifier_event(opts, server_id, err)
-      return
-    end
 
     json_data = data.to_json
     json_opts = opts.to_json
