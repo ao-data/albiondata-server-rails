b941db7062654b7571eda956b4832dc0b4b43939
AlbionFreeMarket <albionfreemarket@albionfreemarket.com>
2026-02-02 02:31:10 -0300

    Add bandit event gating logic and corresponding tests

--- app/services/bandit_event_service.rb
+++ app/services/bandit_event_service.rb
@@ -1,15 +1,60 @@
 class BanditEventService
+  BANDIT_EVENT_MIN_DISTINCT_IPS = ENV.fetch('BANDIT_EVENT_MIN_DISTINCT_IPS', 5).to_i
+  BANDIT_EVENT_IP_TTL_SECONDS = ENV.fetch('BANDIT_EVENT_IP_TTL_SECONDS', 600).to_i
+  CSHARP_TICKS_UNIX_EPOCH = 621355968000000000
+  CSHARP_TICKS_PER_SECOND = 10_000_000
+
   def process(data, server_id, opts)
     log = { class: 'BanditEventService', method: 'process', data: data, server_id: server_id, opts: opts }
     Sidekiq.logger.info(log.to_json)
 
-    json_data = data.to_json
-    json_opts = opts.to_json
+    client_ip = opts.is_a?(Hash) ? (opts['client_ip'] || opts[:client_ip]) : nil
+    event_time = data.key?('EventTime') ? data['EventTime'] : data[:EventTime]
+    advance_notice = data.key?('AdvanceNotice') ? data['AdvanceNotice'] : data[:AdvanceNotice]
 
-    nats = NatsService.new(server_id)
-    nats.send('banditevent.ingest', json_data)
-    nats.close
-    IdentifierService.add_identifier_event(opts, server_id, 'Received on BanditEventService')
+    if client_ip.nil? || event_time.nil? || advance_notice.nil?
+      IdentifierService.add_identifier_event(opts, server_id, 'Received on BanditEventService, missing event fields or client ip, ignored')
+      return
+    end
 
+    parsed_event_time = parse_event_time_ticks(event_time)
+    unless parsed_event_time
+      IdentifierService.add_identifier_event(opts, server_id, 'Received on BanditEventService, invalid EventTime ticks, ignored')
+      return
+    end
+
+    now = Time.now.utc
+    if parsed_event_time < now || parsed_event_time > (now + 5.hours)
+      IdentifierService.add_identifier_event(opts, server_id, 'Received on BanditEventService, EventTime out of allowed window, ignored')
+      return
+    end
+
+    key = "event:#{event_time}:#{advance_notice}:ips"
+    redis = REDIS[server_id]
+    redis.sadd(key, client_ip)
+    redis.expire(key, BANDIT_EVENT_IP_TTL_SECONDS)
+    distinct_ips = redis.scard(key)
+
+    if distinct_ips > BANDIT_EVENT_MIN_DISTINCT_IPS
+      json_data = data.to_json
+      nats = NatsService.new(server_id)
+      nats.send('banditevent.ingest', json_data)
+      nats.close
+      IdentifierService.add_identifier_event(opts, server_id, "Received on BanditEventService, distinct ips #{distinct_ips}, sent to NATS")
+    else
+      IdentifierService.add_identifier_event(opts, server_id, "Received on BanditEventService, distinct ips #{distinct_ips}, below threshold")
+    end
+  end
+
+  private
+
+  def parse_event_time_ticks(ticks)
+    ticks_i = Integer(ticks)
+    return nil if ticks_i < CSHARP_TICKS_UNIX_EPOCH
+
+    seconds = (ticks_i - CSHARP_TICKS_UNIX_EPOCH) / CSHARP_TICKS_PER_SECOND.to_f
+    Time.at(seconds).utc
+  rescue ArgumentError, TypeError, RangeError
+    nil
   end
 end
--- spec/app/services/bandit_event_spec.rb
+++ spec/app/services/bandit_event_spec.rb
@@ -1,13 +1,90 @@
 describe BanditEventService, type: :service do
 
   describe '.process' do
-    let(:data) { { 'EventTime' => 638997538711438026 } }
-    let(:opts) { { 'foo' => 'bar' } }
+    let(:server_id) { 'west' }
+    let(:now) { Time.utc(2026, 2, 2, 12, 0, 0) }
+    let(:event_time_ticks) { BanditEventService::CSHARP_TICKS_UNIX_EPOCH + (now.to_i * BanditEventService::CSHARP_TICKS_PER_SECOND) }
+    let(:data) { { 'EventTime' => event_time_ticks, 'AdvanceNotice' => 1 } }
+    let(:opts) { { 'client_ip' => '1.2.3.4', 'identifier' => 'abc' } }
+    let(:redis) { instance_double(Redis) }
+
+    before do
+      allow(Time).to receive(:now).and_return(now)
+      allow(REDIS).to receive(:[]).with(server_id).and_return(redis)
+      allow(redis).to receive(:sadd)
+      allow(redis).to receive(:expire)
+      allow(redis).to receive(:scard).and_return(0)
+      allow(IdentifierService).to receive(:add_identifier_event)
+    end
 
     it 'sends a log message' do
-      expected_log =  { class: 'BanditEventService', method: 'process', data: data, server_id: 'west', opts: opts }.to_json
+      expected_log =  { class: 'BanditEventService', method: 'process', data: data, server_id: server_id, opts: opts }.to_json
       expect(Sidekiq.logger).to receive(:info).with(expected_log)
-      subject.process(data, 'west', opts)
+      subject.process(data, server_id, opts)
+    end
+
+    it 'sends to NATS when distinct ips exceed threshold' do
+      allow(redis).to receive(:scard).and_return(6)
+      nats = instance_double(NatsService)
+
+      expect(NatsService).to receive(:new).with(server_id).and_return(nats)
+      expect(nats).to receive(:send).with('banditevent.ingest', data.to_json)
+      expect(nats).to receive(:close)
+      expect(IdentifierService).to receive(:add_identifier_event).with(opts, server_id, /sent to NATS/)
+
+      subject.process(data, server_id, opts)
+    end
+
+    it 'does not send to NATS when distinct ips are below threshold' do
+      allow(redis).to receive(:scard).and_return(5)
+
+      expect(NatsService).not_to receive(:new)
+      expect(IdentifierService).to receive(:add_identifier_event).with(opts, server_id, /below threshold/)
+
+      subject.process(data, server_id, opts)
+    end
+
+    it 'accepts false AdvanceNotice values' do
+      data_with_false = { 'EventTime' => event_time_ticks, 'AdvanceNotice' => false }
+
+      expect(redis).to receive(:sadd).with("event:#{event_time_ticks}:false:ips", opts['client_ip'])
+      expect(IdentifierService).to receive(:add_identifier_event).with(opts, server_id, /below threshold/)
+
+      subject.process(data_with_false, server_id, opts)
+    end
+
+    it 'ignores events with invalid EventTime ticks' do
+      bad_data = { 'EventTime' => 'not-a-tick', 'AdvanceNotice' => 1 }
+
+      expect(REDIS).not_to receive(:[])
+      expect(IdentifierService).to receive(:add_identifier_event).with(opts, server_id, /invalid EventTime/)
+
+      subject.process(bad_data, server_id, opts)
+    end
+
+    it 'ignores events with EventTime in the past' do
+      now = Time.utc(2026, 2, 2, 12, 0, 0)
+      allow(Time).to receive(:now).and_return(now)
+      past_ticks = BanditEventService::CSHARP_TICKS_UNIX_EPOCH + ((now.to_i - 1) * BanditEventService::CSHARP_TICKS_PER_SECOND)
+      past_data = { 'EventTime' => past_ticks, 'AdvanceNotice' => 1 }
+
+      expect(REDIS).not_to receive(:[])
+      expect(IdentifierService).to receive(:add_identifier_event).with(opts, server_id, /EventTime out of allowed window/)
+
+      subject.process(past_data, server_id, opts)
+    end
+
+    it 'ignores events with EventTime more than 5 hours ahead' do
+      now = Time.utc(2026, 2, 2, 12, 0, 0)
+      allow(Time).to receive(:now).and_return(now)
+      future_seconds = (now + 6.hours).to_i
+      future_ticks = BanditEventService::CSHARP_TICKS_UNIX_EPOCH + (future_seconds * BanditEventService::CSHARP_TICKS_PER_SECOND)
+      future_data = { 'EventTime' => future_ticks, 'AdvanceNotice' => 1 }
+
+      expect(REDIS).not_to receive(:[])
+      expect(IdentifierService).to receive(:add_identifier_event).with(opts, server_id, /EventTime out of allowed window/)
+
+      subject.process(future_data, server_id, opts)
     end
   end
 end
