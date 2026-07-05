35c18ae995a48c09b31cfcc80762133859e024ac
Paul Hendryx <phendryx@gmail.com>
2026-07-05 12:35:48 -0400

    Feature improvements 20260704 (#110)
    * check record counts when exporting
    
    * test corrections
    
    * add index, add new field that calculates the 5 minute bin time
    
    * db migration stuff
    
    * gha composer fix

--- app/services/market_data_service.rb
+++ app/services/market_data_service.rb
@@ -11,19 +11,31 @@ class MarketDataService
     # Split the provided parameters into separate variables
     ids, locations, qualities = params[:id].upcase.split(',').uniq, get_locations(params), get_qualities(params)
 
-    data = MarketOrder.where(item_id: ids, updated_at: 1.days.ago..)
+    since_bin = 1.day.ago.to_i / 300 * 300
+
+    # Inner subquery: find the latest 5-minute bin per (item, location, quality, auction_type)
+    latest_bins = MarketOrder.where(item_id: ids, updated_at_bin: since_bin..)
+    latest_bins = latest_bins.where(location: locations) unless locations.empty?
+    latest_bins = latest_bins.where(quality_level: qualities) unless qualities.empty?
+    latest_bins = latest_bins
+      .group(:item_id, :location, :quality_level, :auction_type)
+      .select(:item_id, :location, :quality_level, :auction_type, 'MAX(updated_at_bin) AS max_bin')
+
+    # Outer query: join to latest_bins to restrict to only that bin's records, then aggregate
+    data = MarketOrder.joins("INNER JOIN (#{latest_bins.to_sql}) lb
+                                ON market_orders.item_id       = lb.item_id
+                               AND market_orders.location       = lb.location
+                               AND market_orders.quality_level  = lb.quality_level
+                               AND market_orders.auction_type   = lb.auction_type
+                               AND market_orders.updated_at_bin = lb.max_bin")
+                      .where(item_id: ids, updated_at_bin: since_bin..)
     data = data.where(location: locations) unless locations.empty?
     data = data.where(quality_level: qualities) unless qualities.empty?
-    data = data.group('2 asc', '3 desc')
-    data = data.select('CONCAT(item_id, "!!", location, "!!", quality_level) AS o_keey',
-                             'CONCAT(item_id, "!!", location, "!!_", quality_level, "!!", auction_type) AS o_keey_at',
-                             'FROM_UNIXTIME((UNIX_TIMESTAMP(updated_at) DIV 300 * 300), "%Y-%m-%dT%H:%i:%s") AS updated_at_binned',
-                             'min(price) as min_price',
-                             'max(price) as max_price',
-                             'auction_type',
-                             'item_id',
-                             'location',
-                             'quality_level')
+    data = data
+      .group('market_orders.item_id', 'market_orders.location', 'market_orders.quality_level', 'market_orders.auction_type', 'lb.max_bin')
+      .select('market_orders.item_id', 'market_orders.location', 'market_orders.quality_level', 'market_orders.auction_type',
+              'MIN(market_orders.price) AS min_price', 'MAX(market_orders.price) AS max_price',
+              "FROM_UNIXTIME(lb.max_bin, '%Y-%m-%dT%H:%i:%s') AS updated_at_binned")
 
     # build a hash of humanized cities for this call
     humanized_cities = {}
@@ -35,22 +47,16 @@ class MarketDataService
     execution_time = Benchmark.measure do
       rows = MarketOrder.connection.select_rows(data.to_sql)
     end
-    Rails.logger.info("Retriving data took #{execution_time.real} seconds")
+    Rails.logger.info("Retrieving data took #{execution_time.real} seconds")
 
     results = {}
-    default_date = DateTime.new(0001, 1, 1, 0, 0, 0).strftime('%Y-%m-%dT%H:%M:%S')
-    last_o_keey_at = nil
+    default_date = '0001-01-01T00:00:00'
     execution_time = Benchmark.measure do
-      rows.each do |result|
-        o_keey, o_keey_at, updated_at_binned, min_price, max_price, auction_type, item_id, location, quality_level = result
-
+      rows.each do |item_id, location, quality_level, auction_type, min_price, max_price, updated_at_binned|
         humanized_city = humanized_cities[location]
-        city_key = o_keey.gsub("!!#{location.to_s}!!", "!!#{humanized_city}!!")
+        city_key = "#{item_id}!!#{humanized_city}!!#{quality_level}"
 
-        next if last_o_keey_at == o_keey_at
-        last_o_keey_at = o_keey_at
-
-        results[city_key] ||= {item_id: item_id, city: humanized_city, quality: quality_level, sell_price_min: 0, sell_price_min_date: default_date, sell_price_max: 0, sell_price_max_date: default_date, buy_price_min: 0, buy_price_min_date: default_date, buy_price_max: 0, buy_price_max_date: default_date }
+        results[city_key] ||= { item_id: item_id, city: humanized_city, quality: quality_level, sell_price_min: 0, sell_price_min_date: default_date, sell_price_max: 0, sell_price_max_date: default_date, buy_price_min: 0, buy_price_min_date: default_date, buy_price_max: 0, buy_price_max_date: default_date }
         if auction_type == 'offer'
           results[city_key][:sell_price_min] = min_price
           results[city_key][:sell_price_min_date] = updated_at_binned
@@ -69,10 +75,10 @@ class MarketDataService
     # fill in the blanks
     execution_time = Benchmark.measure do
       default_values = { sell_price_min: 0, sell_price_min_date: default_date, sell_price_max: 0, sell_price_max_date: default_date, buy_price_min: 0, buy_price_min_date: default_date, buy_price_max: 0, buy_price_max_date: default_date }
-      location_strings = locations.map { |location| location_to_city(location) }
-      ids.sort.product(location_strings.map{|s| humanize_city(s.to_s)}.sort, qualities.sort).map do |item_id, location, quality|
+      humanized_location_strings = locations.map { |loc| humanized_cities[loc] }
+      ids.sort.product(humanized_location_strings.sort, qualities.sort).each do |item_id, location, quality|
         key = "#{item_id}!!#{location}!!#{quality}"
-        results[key] ||= {item_id: item_id, city: location, quality: quality}.merge(default_values)
+        results[key] ||= { item_id: item_id, city: location, quality: quality }.merge(default_values)
       end
     end
     Rails.logger.info("Blanks processing took #{execution_time.real} seconds")
